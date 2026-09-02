2fe6552fb87d022737d1ebdd25d3e7eb11438270
Peter Damiano <petedianotech@gmail.com>
2026-09-02 06:37:48 +0200

    Add Room Database

---
+++ app/src/main/java/com/todonotepro/app/data/AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.todonotepro.app.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [TodoNoteItem::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "todonotepro.db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
+}
